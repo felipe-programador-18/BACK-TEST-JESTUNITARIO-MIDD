c9b760a3b560615f3df9b0c7c9eb2ff5e3f5de21
Felipe <machadofelipe2016@outlook.com>
2022-03-18 16:41:54 -0300

    c11- create new router called products

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const IndexControllers =  require('../controllers/index')
 
 router.get('/', IndexControllers.Home )
+router.get('/produtos', IndexControllers.Produt)
+
 
 
 module.exports = router
